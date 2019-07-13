1b659dbac060798e2c54effe9c8b395e81d2752d
Janusz Tomasik <januszveiro@gmail.com>
2019-07-13 18:06:30 +0200

    Make transfers thread safe without hibernate

--- src/main/java/com/vrq/revolut/resources/TransferResource.java
+++ src/main/java/com/vrq/revolut/resources/TransferResource.java
@@ -5,17 +5,14 @@ import com.vrq.revolut.core.Account;
 import com.vrq.revolut.core.Transfer;
 import com.vrq.revolut.db.AccountDao;
 import com.vrq.revolut.db.TransferDao;
-import io.dropwizard.hibernate.UnitOfWork;
 
 import javax.validation.Valid;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.vrq.revolut.service.TransferService.performTransfer;
 import static com.vrq.revolut.service.TransferService.validate;
@@ -26,6 +23,7 @@ public class TransferResource {
 
     private final TransferDao transferDao;
     private final AccountDao accountDao;
+    private final Map<Long, ReentrantLock> accountLocks = new ConcurrentHashMap<>();
 
     public TransferResource(TransferDao transferDao, AccountDao accountDao) {
         this.transferDao = transferDao;
@@ -34,7 +32,29 @@ public class TransferResource {
 
     @POST
     public Transfer add(@Valid Transfer transfer) {
+        long fromAccountId = transfer.getFromAccount().getId();
+        long toAccountId = transfer.getToAccount().getId();
+        accountLocks.putIfAbsent(fromAccountId, new ReentrantLock());
+        accountLocks.putIfAbsent(toAccountId, new ReentrantLock());
 
+        if (fromAccountId < toAccountId) {
+            synchronized (accountLocks.get(fromAccountId)) {
+                synchronized (accountLocks.get(toAccountId)) {
+                    return perform(transfer);
+                }
+            }
+        } else if (fromAccountId > toAccountId) {
+            synchronized (accountLocks.get(toAccountId)) {
+                synchronized (accountLocks.get(fromAccountId)) {
+                    return perform(transfer);
+                }
+            }
+        } else {
+            throw new WebApplicationException("Cannot make transfers between the same account");
+        }
+    }
+
+    private Transfer perform(@Valid Transfer transfer) {
         Account fromAccount = accountDao.findById(transfer.getFromAccount().getId());
         Account toAccount = accountDao.findById(transfer.getToAccount().getId());
         validate(transfer, fromAccount);
